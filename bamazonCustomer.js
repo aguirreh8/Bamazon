2ac346c250fb97b25bdeaab068ac78e9af7c8136
unknown <aguirreh8@gmail.com>
2018-02-21 22:45:01 -0500

    added commitTransaction and continueShopping functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,18 +13,20 @@ const connection = mysql.createConnection({
 });
 
 const runUserQuery = (query) => {
+	const choicesArray = [];
+	console.log("~~~~~~~~~~~ Welcome to Bamazon! Please take a log at our wares:");
+	console.log(`ID\t| Product Name| Price | Quantity`);
+	for (let i = 0; i < query.length; i++) {
+		console.log(`${query[i].item_id}\t| ${query[i].product_name} | ${query[i].price} | ${query[i].stock_quantity}`);
+		choicesArray.push(JSON.stringify(query[i].item_id));
+	}
+
 	inquirer.prompt([
 		{
 			name: "userSelection",
 			message: "What item would you like to buy (use the item ID to select)?",
 			type: "list",
-			choices: function() {
-				const choicesArray = [];
-				for (let i = 0; i < query.length; i++) {
-					choicesArray.push(`${query[i].item_id} | ${query[i].product_name} | ${query[i].price}`);
-				}
-				return choicesArray;
-			}
+			choices: choicesArray
 		},
 		{
 			name: "buyQuantity",
@@ -33,8 +35,8 @@ const runUserQuery = (query) => {
 		}
 	]).then(response => {
 		const quantity = parseInt(response.buyQuantity);
-		console.log(`You have selected ${response.userSelection} and bought ${quantity} of them`);
-		connection.end();
+		const itemID = parseInt(response.userSelection);
+		commitTranscation(itemID, quantity);
 	})
 }
 
@@ -46,5 +48,43 @@ const readAll = () => {
 		}
 	)
 }
+
+const commitTranscation = (itemID, quantity) => {
+	connection.query(
+		"UPDATE products SET stock_quantity = (stock_quantity - ?) WHERE item_id = ?",
+		[quantity, itemID],
+		function(error) {
+			if (error) throw error;;
+		});
+
+	connection.query(
+		"SELECT product_name, price FROM products WHERE item_id = ?",
+		[itemID],
+		function(error, results) {
+			let total = quantity * results[0].price;
+			console.log(`Thank you for buying ${quantity} ${results[0].product_name}(s) for $${total}.`);
+			continueShopping();
+		}
+	)
+};
+
+
+const continueShopping = () => {
+		inquirer.prompt([
+		{
+			name: "continue",
+			message: "Would you like to keep shopping?",
+			type: "confirm"
+		}
+	]).then(response => {
+		if (response.continue) {
+			readAll();
+		} else {
+			console.log("Come again!");
+			connection.end();
+		}
+	})
+}
+
 
 readAll();
